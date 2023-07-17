331a39771dff7d1b460f478f8e4e7c1c1ee8dadc
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-07-17 01:24:01 -0500

    cleaned up the javascript by adding a math random to choose the categories then inserting that category into the url for the quote API. I also added an extra event listener to clear the local storage for the refresh button.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,41 +1,25 @@
 var quoteCat;
 $(function quotes() {
-    var category = 'happiness'
-    var category2 = 'anger'
+    var categories = ["happiness", "anger"];
+    var mood = Math.floor(Math.random() * categories.length)
     $.ajax({
         method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+        url: 'https://api.api-ninjas.com/v1/quotes?category=' + categories[mood],
         headers: { 'X-Api-Key': 'zanloHQp53FvGJffeRPwng==0BaPHaRW4myCo1F3'},
         contentType: 'application/json',
         success: function(result) {
-            console.log(result[0].quote);
+            console.log(result[0].category);
             $('#quote').text(result[0].quote + '  - ' + result[0].author);
-            localStorage.setItem("quoteCat", JSON.stringify(result[0].category));
+          var setQuote =  localStorage.setItem("quoteCat", JSON.stringify(result[0].category));
+          console.log(setQuote)
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
         }
     });
     
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/quotes?category=' + category2,
-        headers: { 'X-Api-Key': 'zanloHQp53FvGJffeRPwng==0BaPHaRW4myCo1F3'},
-        contentType: 'application/json',
-        success: function(result) {
-            console.log(result);
-            $('#quote').text(result[0].quote + '  - ' + result[0].author);
-        },
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
-    });
     });
 
-// GET https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=UC8TZwtZ17WKFJSmwTZQpBTA&maxResults=25&key=[YOUR_API_KEY] HTTP/1.1
-
-// Authorization: Bearer [YOUR_ACCESS_TOKEN]
-// Accept: application/json
 
 ytApiKey = 'AIzaSyAAf6fRHMjxIPj6MQ4fr2jDvzw-VDIUy3o'
 
@@ -44,16 +28,18 @@ $.ajax({
     url: 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLU6EQu9809AJYvaOtXnB61QoYrUshDw-F&key=' + ytApiKey,
     contentType: 'application/json',
     success: function(result) { 
-        console.log(result.items[0].snippet.resourceId.videoId);
-        var happy = result.items[0].snippet.resourceId.videoId
-        var anger = result.items[1].snippet.resourceId.videoId
+        console.log(result.items[1].snippet.resourceId.videoId);
+        var happy = result.items[0].snippet.resourceId.videoId;
+        var anger = result.items[1].snippet.resourceId.videoId;
        var quoteCat = localStorage.getItem("quoteCat");
        console.log(quoteCat);
        if (quoteCat === "happiness") {
-        $('#ytVid').attr("src", "https://youtube.com/embed/" + happy);
+       var happyUrl = $('#ytVid').attr("src", "https://youtube.com/embed/" + happy);
+       console.log(happyUrl)
        }
        else{
-        $('#ytVid').attr("src", "https://youtube.com/embed/" + anger)
+        var angerUrl = $('#ytVid').attr("src", "https://youtube.com/embed/" + anger);
+        console.log(angerUrl);
        }
     
     },
@@ -61,33 +47,6 @@ $.ajax({
         console.error('Error: ', jqXHR.responseText);
     }
 });
-// 
-// we want to store the category of the quote submitted to local store
-// then in a seperate function get the category happiness or angry
-// assign each category with its respective result number
-// append results to page 
-
-//   function loadClient() {
-//     gapi.client.setApiKey("AIzaSyAAf6fRHMjxIPj6MQ4fr2jDvzw-VDIUy3o");
-//     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-//         .then(function() { console.log("GAPI client loaded for API"); },
-//               function(err) { console.error("Error loading GAPI client for API", err); });
-//   }
-//   // Make sure the client is loaded and sign-in is complete before calling this method.
-//   function execute() {
-//     return gapi.client.youtube.playlists.list({
-//       "part": [
-//         "snippet,contentDetails"
-//       ],
-//       "channelId": "UC8TZwtZ17WKFJSmwTZQpBTA",
-//       "maxResults": 25
-//     })
-//         .then(function(response) {
-//                 // Handle the results here (response.result has the parsed body).
-//                 console.log("Response", response);
-//               },
-//               function(err) { console.error("Execute error", err); });
-//   }
 
 $("#submit").click(function(){
     window.location.href='results.html';
@@ -95,5 +54,10 @@ $("#submit").click(function(){
  });
 
 $('#back').click(function(){
-    window.location.href='index.html';
+    window.location.href='index.html'; 
  });
+ $('#refresh').click(function(){
+    localStorage.clear();
+    quotes();
+ })
+
