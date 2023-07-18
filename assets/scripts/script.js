4475321432c0528da91c28b0e6ed11cf071bad38
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-07-18 15:51:10 -0500

    added more categories and just put them in the if expressions but could be cleaner with a for loop. also deleted the second page and just added hidden styles to the results and actions upon the click event of the submit button because the bug of it rerunning the function of quotes on the second page is a problem i couldn't fix in script but could fix by sending everything to one html page.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,5 @@
 function quotes() {
-    var categories = ["happiness", "anger"];
+    var categories = ["happiness", "anger", "dreams", "intelligence", "love"];
     var mood = Math.floor(Math.random() * categories.length);
     $.ajax({
         method: 'GET',
@@ -18,10 +18,11 @@ function quotes() {
     });
 };
 
-ytApiKey = 'AIzaSyAAf6fRHMjxIPj6MQ4fr2jDvzw-VDIUy3o'
+quotes();
 
 function ytConnect(mood) {
     console.log(mood);
+    ytApiKey = 'AIzaSyAAf6fRHMjxIPj6MQ4fr2jDvzw-VDIUy3o'
     $.ajax({
         method: 'GET',
         url: 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLU6EQu9809AJYvaOtXnB61QoYrUshDw-F&key=' + ytApiKey,
@@ -29,8 +30,20 @@ function ytConnect(mood) {
         success: function (result) {
             var happy = result.items[0].snippet.resourceId.videoId;
             var anger = result.items[1].snippet.resourceId.videoId;
+            var dreamy = result.items[2].snippet.resourceId.videoId;
+            var intelligent = result.items[3].snippet.resourceId.videoId;
+            var love = result.items[4].snippet.resourceId.videoId;
             if (mood === "happiness") {
                 $('#ytVid').attr("src", "https://youtube.com/embed/" + happy);
+            }
+            else if (mood === "dreams") {
+                $('#ytVid').attr('src', "https://youtube.com/embed/" + dreamy);
+            }
+            else if (mood === "intelligence") {
+                $('#ytVid').attr('src', "https://youtube.com/embed/" + intelligent);
+            }
+            else if (mood === "love") {
+                $('#ytVid').attr('src', "https://youtube.com/embed/" + love);
             }
             else {
                 $('#ytVid').attr("src", "https://youtube.com/embed/" + anger);
@@ -43,8 +56,13 @@ function ytConnect(mood) {
 };
 
 $("#submit").click(function () {
-    window.location.href = 'results.html';
-    return false;
+    $('#results').attr("style", "display: flex");
+    $('#home-page').attr("style", "display: none");
+    $('#submit').attr("style", "display: none");
+    $('#refresh').attr("style", "display: none");
+    $('#back-btn').attr("style", "display: flex");
+    $('#image-box').attr("style", "display: none");
+    $('#header-subtitle').text("🎶Enjoy Your Playlist!🎶");
 });
 
 $('#back').click(function () {
@@ -54,4 +72,3 @@ $('#refresh').click(function () {
     window.location.reload();
 });
 
-quotes();
